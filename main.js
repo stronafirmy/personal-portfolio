917445ba8f106d65e97a12dba75ab6de7e7126b6
MonaNaamani <134978312+MonaNaamani@users.noreply.github.com>
2024-05-14 18:11:01 +0300

    Add Js for next btn

--- main.js
+++ main.js
@@ -46,10 +46,15 @@ function prevSong () {
   playSong()
 }
 
-
-
 function nextSong () {
+songIndex++
 
+  if(songIndex > songs.length - 1 ) {
+    songIndex = 0
+  }
+
+  loadSong(songs[songIndex])
+  playSong()
 }
 
 // Event listeners
