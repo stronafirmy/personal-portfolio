1ff534cc1df6e13207220955c34f4540b4310c61
MonaNaamani <134978312+MonaNaamani@users.noreply.github.com>
2024-05-15 16:16:34 +0300

    Fix scroll hash issue

--- main.js
+++ main.js
@@ -109,5 +109,3 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el))
-
-window.addEventListener("hashchange", function() { scrollBy(0, -100) })
