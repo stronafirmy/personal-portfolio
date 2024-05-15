88b78d47d06403ff88b44167995946fedd73688e
MonaNaamani <134978312+MonaNaamani@users.noreply.github.com>
2024-05-15 05:37:07 +0300

    Add scroll animations

--- main.js
+++ main.js
@@ -93,3 +93,21 @@ audio.addEventListener('timeupdate', updateProgress);
 progressContainer.addEventListener('click', setProgress)
 
 audio.addEventListener('ended', nextSong)
+
+// Scroll Animations
+
+const observer = new IntersectionObserver((entries) => {
+  entries .forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show')
+    } else {
+      entry.target.classList.remove('show')
+    }
+  })
+})
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el))
+
+window.addEventListener("hashchange", function() { scrollBy(0, -100) })
