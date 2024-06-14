5eb5825b5466e32cb28f368c41a18fe090f653fd
MonaNaamani <134978312+MonaNaamani@users.noreply.github.com>
2024-06-15 01:31:17 +0300

    Fix image paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://monanaamani.github.io',
-  base: 'personal-portfolio',
+  base: '/personal-portfolio',
 });
